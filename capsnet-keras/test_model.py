e9ca6f0ced7e0881961077ff0058a5cb87d0307a
grassknoted <grassknoted@gmail.com>
2018-11-14 01:48:32 +0530

    inverse class list added

--- capsnet-keras/test_model.py
+++ capsnet-keras/test_model.py
@@ -1,5 +1,11 @@
 from keras.models import load_model
 
 save_dir = './result'
+dataset_path = "../Dataset/Animals/"
 
-model = load_model(save_dir + '/trained_model.h5')
+inverse_class_dict = {0:'Cat', 1:"Dog", 2:"Fox", 3:"Hyena", 4:"Wolves"}
+
+model = load_model(save_dir + '/weights-507.h5')
+
+prediction = model.predict(dataset_path+'/dogs/dog1.jpg')
+print("Predicted as: ", prediction)
